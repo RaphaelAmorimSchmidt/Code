d73b3ca1b96bbacea989c05a05431129f340535e
Raphael Amorim Schmidt <raphaschmidt89@gmail.com>
2024-03-17 15:26:02 -0300

    first python script

---
+++ Python/Olá Mundo!.py
@@ -0,0 +1,9 @@
+print('Olá Mundo!\n')
+print('Esse é o meu programa!')
+nome = "Raphael Amorim Schmidt"
+idade = 34
+print("meu nome é "+nome+" e minha idade é",idade)
+numero1 = int(input("primeiro número: "))
+numero2 = int(input("segundo número: "))
+soma = numero1 + numero2
+print("a soma é ",soma)
