435c36d9746c66ef9d5227118ff7f66e09f2d68b
Raphael Amorim Schmidt <raphaschmidt89@gmail.com>
2024-03-17 16:53:04 -0300

    .isnumeric()

--- Python/Olá Mundo!.py
+++ Python/Olá Mundo!.py
@@ -3,7 +3,11 @@ print('Esse é o meu programa!')
 nome = "Raphael Amorim Schmidt"
 idade = 34
 print("meu nome é "+nome+" e minha idade é",idade)
+print('*'*15)
 numero1 = int(input("primeiro número: "))
 numero2 = int(input("segundo número: "))
 soma = numero1 + numero2
 print( 'a soma entre {} e {} é {}'.format( numero1, numero2, soma ) )
+print ('-'*15)
+algo = input("Digite algo: ")
+print('O digitado só tem numeros? {}\nO digitado só tem letras? {}\nO digitado está maiusculo? {} '.format(str(algo.isnumeric()), str(algo.isalpha()), str(algo.isupper())))
