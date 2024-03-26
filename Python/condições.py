eaa8d96d26e288b831e2e5ba64ef152a688d2ab9
Raphael Amorim Schmidt <raphaschmidt89@gmail.com>
2024-03-25 23:23:27 -0300

    jokenpo

--- Python/condições.py
+++ Python/condições.py
@@ -1,3 +1,5 @@
+import emoji
+import random
 nome = str(input('Qual é o seu nome? '))
 if nome == 'Raphael':
     print('Olá Raphael! Bom dia')
@@ -18,3 +20,32 @@ if aprovacao:
     print('Parabéns seu empréstimo foi aprovado no valor de R${:.2f} ao mês'.format(prestacao))
 else:
     print('Infelizmente seu empréstimo foi recusado')
+print('-*'*15)
+print('-*'*15)
+print('Vamos jogar JoKenPô!!!!')
+print('Escolha uma das três opções:')
+print(emoji.emojize('1 - para pedra :raised_fist:'))
+print('2 - para tesoura ✌')
+print('3 - para papel 🖐')
+escolha = int(input( 'Qual você quer: '))
+if escolha == 1:
+    emoesc = '✊'
+elif escolha == 2:
+    emoesc = '✌'
+else:
+    emoesc = '🖐'
+pcescolha = random.randint(1,4)
+if pcescolha == 1:
+    emopcesc = '✊'
+elif pcescolha == 2:
+    emopcesc = '✌'
+else:
+    emopcesc = '🖐'
+
+if escolha == pcescolha:
+    print('Empate!!!!')
+elif (escolha == 1 and pcescolha == 2) or (escolha == 2 and pcescolha == 3) or (escolha == 3 and pcescolha == 1):
+    print('Parabéns você ganhou!!!!!')
+else:
+    print('Você perdeu!!!!!')
+print(emoji.emojize('{} {}'.format(emoesc,emopcesc)))
