ee8cc3398818ab1afe492756148715244f9173b8
Raphael Amorim Schmidt <raphaschmidt89@gmail.com>
2024-03-30 20:22:35 -0300

    segunda parte

--- Python/desafio misto.py
+++ Python/desafio misto.py
@@ -1,11 +1,23 @@
 resp = 'S'
 c = 0
 total = 0
+maior = 0
+menor = 0
 while(resp not in 'Nn'):
     resp = input('Você quer acrescentar um número para calcular a média? Se sim digite o número, se não digite N: ')
     if(resp.isnumeric()):
         c += 1
-        total += int(resp)
+        resp = int(resp)
+        total += resp
+        if c == 1:
+            maior = resp
+            menor = resp
+        else:
+            if maior < resp:
+                maior = resp
+            if menor > resp:
+                menor = resp
+    resp = str(resp)
 media = total/c
-print('A média ficou em {} entre os {} elementos.'.format(media, c))
+print('A média ficou em {} entre os {} elementos. O maior núemro foi {} e o menor foi {}'.format(media, c, maior, menor))
         
